5b23957769eb3369e77445674e863a277cec5c01
lutencjusz <lutencjusz@gmail.com>
2024-03-24 15:25:10 +0100

    wprowadzenie danych testowych

--- src/main/java/TicTacToeNeuralNetwork.java
+++ src/main/java/TicTacToeNeuralNetwork.java
@@ -7,7 +7,99 @@ import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
+import java.util.Scanner;
+
 public class TicTacToeNeuralNetwork {
+    // Metoda do wizualizacji planszy
+    public static void displayBoard(double[] board) {
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0) {
+                System.out.println();
+            }
+            if (board[i] == 1) {
+                System.out.print("X ");
+            } else if (board[i] == -1) {
+                System.out.print("O ");
+            } else {
+                System.out.print(". ");
+            }
+        }
+        System.out.println();
+    }
+
+    // Metoda do gry
+    public static void playGame(BasicNetwork network) {
+        Scanner scanner = new Scanner(System.in);
+        double[] board = new double[9]; // Początkowy stan planszy
+
+        while (true) {
+            // Gracz wprowadza ruch
+            System.out.println("Podaj współrzędne ruchu (0-8): ");
+            int move = scanner.nextInt();
+            board[move] = 1; // Zakładamy, że gracz jest reprezentowany przez 1
+
+            // Wyświetlanie planszy
+            displayBoard(board);
+
+            // Sprawdzanie, czy gracz wygrał
+            if (checkWin(board, 1)) {
+                System.out.println("Wygrałeś!");
+                break;
+            }
+
+            // Sieć neuronowa oblicza ruch
+            MLData inputMLData = new BasicMLData(board);
+            MLData outputMLData = network.compute(inputMLData);
+            int networkMove = getBestMove(outputMLData.getData());
+            board[networkMove] = -1; // Zakładamy, że sieć neuronowa jest reprezentowana przez -1
+
+            // Wyświetlanie planszy
+            displayBoard(board);
+
+            // Sprawdzanie, czy sieć neuronowa wygrała
+            if (checkWin(board, -1)) {
+                System.out.println("Sieć neuronowa wygrała!");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static int getBestMove(double[] data) {
+        int bestMove = 0;
+        double bestValue = Double.MIN_VALUE;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > bestValue) {
+                bestValue = data[i];
+                bestMove = i;
+            }
+        }
+        return bestMove;
+    }
+
+    private static boolean checkWin(double[] board, int i) {
+        // Sprawdzanie wierszy
+        for (int j = 0; j < 3; j++) {
+            if (board[j] == i && board[j + 3] == i && board[j + 6] == i) {
+                return true;
+            }
+        }
+
+        // Sprawdzanie kolumn
+        for (int j = 0; j < 9; j += 3) {
+            if (board[j] == i && board[j + 1] == i && board[j + 2] == i) {
+                return true;
+            }
+        }
+
+        // Sprawdzanie przekątnych
+        if (board[0] == i && board[4] == i && board[8] == i) {
+            return true;
+        }
+        return board[2] == i && board[4] == i && board[6] == i;
+    }
+
     public static void main(String[] args) {
         // Tworzenie sieci neuronowej
         BasicNetwork network = new BasicNetwork();
@@ -19,19 +111,19 @@ public class TicTacToeNeuralNetwork {
 
         // Dane treningowe (plansze i oczekiwane wyjścia)
         double[][] input = {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0}, // Plansza pusta
-                // Dodaj więcej plansz treningowych
-        };
-        double[][] output = {
-                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
-                // Dodaj więcej oczekiwanych wyjść
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}, // Pusta plansza
+                {1, 0, 0, 0, 0, 0, 0, 0, 0}, // Krzyżyk w lewym górnym rogu
+                {0, 1, 0, 0, 0, 0, 0, 0, 0}, // Krzyżyk na górze na środku
+                {0, 0, 1, 0, 0, 0, 0, 0, 0}, // Krzyżyk w prawym górnym rogu
+                {0, 0, 0, 1, 0, 0, 0, 0, 0}, // Krzyżyk w lewym środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Krzyżyk na środku
+                {0, 0, 0, 0, 0, 1, 0, 0, 0}, // Krzyżyk w prawym środku
+                {0, 0, 0, 0, 0, 0, 1, 0, 0}, // Krzyżyk w lewym dolnym rogu
+                {0, 0, 0, 0, 0, 0, 0, 1, 0}, // Krzyżyk na dole na środku
+                {0, 0, 0, 0, 0, 0, 0, 0, 1}, // Krzyżyk w prawym dolnym rogu
         };
 
-        // Przygotowanie danych treningowych
-        BasicMLDataSet trainingSet = new BasicMLDataSet(input, output);
-
-        // Trenowanie sieci
-        ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+        final ResilientPropagation train = getResilientPropagation(input, network);
         int epoch = 1;
         do {
             train.iteration();
@@ -44,6 +136,34 @@ public class TicTacToeNeuralNetwork {
         MLData outputMLData = network.compute(inputMLData);
         System.out.println("Wynik: " + outputMLData.getData(4)); // Przykładowe pole na planszy
 
+        // Wyświetlanie planszy
+        displayBoard(input[0]);
+
+        // Rozpoczęcie gry
+        playGame(network);
+
         Encog.getInstance().shutdown();
+    }
+
+    private static ResilientPropagation getResilientPropagation(double[][] input, BasicNetwork network) {
+        double[][] output = {
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {1, 0, 0, 0, 0, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+                {0, 0, 0, 0, 1, 0, 0, 0, 0}, // Oczekiwane wyjście dla krzyżyka na środku
+        };
+
+        // Przygotowanie danych treningowych
+        BasicMLDataSet trainingSet = new BasicMLDataSet(input, output);
+
+        // Trenowanie sieci
+        ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+        return train;
     }
 }
